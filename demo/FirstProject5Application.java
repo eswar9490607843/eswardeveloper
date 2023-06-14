c0ac67976b0211da0d4d854639857dcdd42eb1de
eswar9490607843 <132335784+eswar9490607843@users.noreply.github.com>
2023-06-14 09:56:22 +0530

    Add files via upload

---
+++ demo/FirstProject5Application.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@SpringBootApplication
+public class FirstProject5Application {
+
+	public static void main(String[] args) {
+	  SpringApplication.run(FirstProject5Application.class, args);
+	}
+
+}
